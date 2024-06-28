d891d287346c573d25b75d8cbc82ccc23b4c9e30
ninja03jod <149457974+ninja03jod@users.noreply.github.com>
2024-06-28 19:17:15 +0530

    fix errors

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ preprocessor = Pipeline(steps=[
 ])
 
 # read the training data
-train = pd.read_csv(r'C:\Users\admin\OneDrive\Desktop\Flights_SageMaker_Project\train.csv')
+train = pd.read_csv('train.csv')
 X_train = train.drop(columns="price")
 y_train = train.price.copy()
 
